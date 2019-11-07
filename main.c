c2cf11d6ffb2b5bf0b0e393e3c3b9794ee672118
jindandelion <gieoun524@sookmyung.ac.kr>
2019-11-07 16:12:32 +0900

    Practice5

--- main.c
+++ main.c
@@ -3,20 +3,33 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 /*************
-call by reference
+Practice5
 *************/
-void swap(int*x, int*y)
-{
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-}
+
 void main(void)
 {
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a:%i,b:%i\n",a,b);	
+	int i;
+	int grade[5];
+	int average=0;
+	int result=0;
+	int *gptr;
 	
+	for(i=0;i<5;i++)
+	{
+		printf("input a grade[%i]=",i);
+		scanf("%d",&grade[i]);
+	}
+	gptr=grade;
+	
+	for(i=0;i<5;i++)
+		printf("grade[%d]=%d\n",i,*(gptr+i));
+		
+	for(i=0;i<5;i++)
+	{
+		result=result+*(gptr+i);
+	}
+	average=result/5;
+	
+	printf("\n");
+	printf("average:%d\n",average);
 }
